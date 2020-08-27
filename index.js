f70ad208aee65ff424dea4e5530b175c6d12dae6
jeff-sexton <sexton.jeffp@gmail.com>
2020-08-26 17:21:21 -0700

    Create constructBodyDigestSignature

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fp = require('fastify-plugin')
 const { plugin } = require('./lib')
 
 module.exports = fp(function (fastify, options, next) {
-  fastify.addHook('onRequest', plugin(options))
+  fastify.addHook('preValidation', plugin(options))
   next()
 }, {
   name: 'fastify-hmac'
