4de1ba2e16b16c5e72f03f8cf3bf6d2d3d5ede0a
Edward Stone <ed@autotelic.com>
2024-01-21 12:47:56 -0800

    updated decorator to accept secret as arg

--- index.test.js
+++ index.test.js
@@ -13,7 +13,7 @@ const defaultOptions = {
   }
 }
 
-test('fastifyHMAC plugin - request decorator, without hook', async ({ is, same, teardown }) => {
+test('request decorator, without hook', async ({ equal, same, teardown }) => {
   teardown(async () => fastify.close())
   const fastify = require('fastify')()
 
@@ -30,12 +30,12 @@ test('fastifyHMAC plugin - request decorator, without hook', async ({ is, same, 
     url: '/test'
   })
 
-  is(fastify.hasRequestDecorator('validateHMAC'), true)
-  is(response.statusCode, 200)
+  equal(fastify.hasRequestDecorator('validateHMAC'), true)
+  equal(response.statusCode, 200)
   same(response.json(), { hmacVerified: { message: 'Signature verification failed' } })
 })
 
-test('fastifyHMAC plugin - hook enabled, request authorized', async ({ is, same, teardown }) => {
+test('hook enabled, request authorized', async ({ equal, same, teardown }) => {
   teardown(async () => fastify.close())
   const fastify = require('fastify')()
 
@@ -55,11 +55,11 @@ test('fastifyHMAC plugin - hook enabled, request authorized', async ({ is, same,
     }
   })
 
-  is(response.statusCode, 200)
+  equal(response.statusCode, 200)
   same(response.json(), { hmacVerified: true })
 })
 
-test('fastifyHMAC plugin - hook enabled, request unauthorized', async ({ is, teardown }) => {
+test('hook enabled, request unauthorized', async ({ equal, teardown }) => {
   teardown(async () => fastify.close())
   const fastify = require('fastify')()
 
@@ -79,16 +79,118 @@ test('fastifyHMAC plugin - hook enabled, request unauthorized', async ({ is, tea
     }
   })
 
-  is(response.statusCode, 401)
-  is(response.json().error, 'Unauthorized')
-  is(response.json().message, 'Signature verification failed')
+  equal(response.statusCode, 401)
+  equal(response.json().error, 'Unauthorized')
+  equal(response.json().message, 'Signature verification failed')
 })
 
-test('fastifyHMAC plugin - no sharedSecret', async ({ rejects, teardown }) => {
+test('fastifyHMAC plugin - no sharedSecret, hook enabled', async ({ rejects, teardown }) => {
   teardown(async () => fastify.close())
   const fastify = require('fastify')()
 
-  fastify.register(fastifyHMAC, {})
+  const options = {
+    ...defaultOptions
+  }
+  delete options.sharedSecret
 
-  await rejects(fastify.ready(), Error('missing shared secret'))
+  fastify.register(fastifyHMAC, options)
+
+  await rejects(fastify.ready())
+})
+
+test('no sharedSecret in options, hook not enabled, secret not passed in decorator', async ({ equal, has, teardown }) => {
+  teardown(async () => fastify.close())
+  const fastify = require('fastify')()
+
+  const options = {
+    ...defaultOptions,
+    validateRequests: false
+  }
+  delete options.sharedSecret
+
+  fastify.register(fastifyHMAC, options)
+
+  fastify.get('/test', (request, reply) => {
+    const validated = request.validateHMAC()
+    reply.send({ hmacVerified: validated })
+  })
+
+  await fastify.ready()
+
+  const response = await fastify.inject({
+    method: 'GET',
+    url: '/test'
+  })
+
+  equal(fastify.hasRequestDecorator('validateHMAC'), true)
+  equal(response.statusCode, 400)
+  has(response.json(), { message: 'No shared secret provided' })
+})
+
+test('no sharedSecret in options, hook not enabled, secret passed in decorator, authorized', async ({ equal, same, teardown }) => {
+  teardown(async () => fastify.close())
+  const fastify = require('fastify')()
+
+  const options = {
+    ...defaultOptions,
+    validateRequests: false
+  }
+  delete options.sharedSecret
+
+  fastify.register(fastifyHMAC, options)
+
+  fastify.get('/foo', (request, reply) => {
+    const validated = request.validateHMAC('test')
+    reply.send({ hmacVerified: validated })
+  })
+
+  await fastify.ready()
+
+  const response = await fastify.inject({
+    method: 'GET',
+    url: '/foo',
+    headers: {
+      signature: 'keyId="test-key-a", algorithm="hs2019", headers="(request-target)", signature="8zJ7k7Cp4Gqfwfe2SYl6u3uqdUfa6PgZZ7Z0+e5+gV/3/UNUyQowMcbEb9Ni0MfGrDGZ9a04RJrIVcZNa0easA=="'
+    }
+  })
+
+  equal(fastify.hasRequestDecorator('validateHMAC'), true)
+  equal(response.statusCode, 200)
+  same(response.json(), { hmacVerified: true })
+})
+
+test('no sharedSecret in options, hook not enabled, secret passed in decorator, not authorized', async ({ equal, same, teardown }) => {
+  teardown(async () => fastify.close())
+  const fastify = require('fastify')()
+
+  const options = {
+    ...defaultOptions,
+    validateRequests: false
+  }
+  delete options.sharedSecret
+
+  fastify.register(fastifyHMAC, options)
+
+  fastify.get('/foo', (request, reply) => {
+    const validated = request.validateHMAC('test')
+    reply.send({ hmacVerified: validated })
+  })
+
+  await fastify.ready()
+
+  const response = await fastify.inject({
+    method: 'GET',
+    url: '/foo',
+    headers: {
+      signature: 'keyId="test-key-a", algorithm="hs2019", headers="(request-target)", signature="vivwFqXDfZ/SlUImUIyHuA2kMZAdsNoTQZTAZvjCHFEMBk4Gq7LIWL50Dfr3StO0+UUofTc2bFS9R68JDK+UfQ=="'
+    }
+  })
+
+  equal(fastify.hasRequestDecorator('validateHMAC'), true)
+  equal(response.statusCode, 200)
+  same(response.json(), {
+    hmacVerified: {
+      message: 'Signature verification failed'
+    }
+  })
 })
